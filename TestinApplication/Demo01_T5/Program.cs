4ce218e75a0201e94e34f6dc6d203e1338764fb5
Oskari Sutela <K2048@student.jamk.fi>
2016-01-20 10:18:39 +0200

    Teht 5 tehty

---
+++ TestinApplication/Demo01_T5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01_T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna aikamääre sekunteina: ");
+            string line = Console.ReadLine(); // "50"
+            int sekuntit = int.Parse(line); // 50
+            int a;
+            int tunnit = sekuntit / 3600;
+            int minuutit = sekuntit / 60 - tunnit * 60;
+            int sekunnit = sekuntit % 60;
+
+
+            Console.WriteLine("Time: {0}h {1}m {2}s", tunnit, minuutit, sekunnit);
+
+            Console.WriteLine("Press enter key to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
